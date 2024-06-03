01f1f9e290ab225c3d9e666abf6271fa2d6c3273
tkalwad1 <146018909+tkalwad1@users.noreply.github.com>
2024-06-03 16:10:28 -0400

    Update and rename isbn_verifier.h to luhn.h

---
+++ luhn.h
@@ -0,0 +1,11 @@
+#if !defined(LUHN_H)
+#define LUHN_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace luhn {
+    bool valid(string ID);
+}  // namespace luhn
+
+#endif // LUHN_H
